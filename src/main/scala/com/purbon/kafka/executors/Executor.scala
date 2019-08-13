28318feef6debbf6a09d8fdd5d91897c20db07c9
Pere Urbon <pere@confluent.io>
2019-08-13 18:23:52 +0200

    clean up interaction with external systems in their own classes

---
+++ src/main/scala/com/purbon/kafka/executors/Executor.scala
@@ -0,0 +1,83 @@
+package com.purbon.kafka.executors
+
+import java.util.Collections
+
+import com.purbon.kafka.readers.{BrokerChangeRequest, SchemaRegistrySingleChangeRequest}
+import com.purbon.kafka.{FileStatusKeeper, SchemaRegistryClient}
+import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
+import org.json4s.JValue
+import org.json4s.native.JsonMethods.parse
+
+
+trait Executor {
+
+}
+
+object SchemaRegistryExecutor {
+
+  val REGISTER_SCHEMA_ACTION: String = "register"
+  val DELETE_SCHEMA_ACTION: String = "delete"
+  val SET_COMPAT_LEVEL_ACTION: String = "set_top_level_compatibility"
+
+  def apply(srClient: SchemaRegistryClient, fileStatusKeeper: FileStatusKeeper): SchemaRegistryExecutor = {
+    new SchemaRegistryExecutor(srClient, fileStatusKeeper)
+  }
+
+}
+
+class SchemaRegistryExecutor(srClient: SchemaRegistryClient, fileStatusKeeper: FileStatusKeeper ) extends Executor {
+
+
+  import SchemaRegistryExecutor._
+
+  def execute(changeRequest: SchemaRegistrySingleChangeRequest): Unit = {
+    val responseJsonText: String = changeRequest.action match {
+      case REGISTER_SCHEMA_ACTION => {
+        srClient.addSchema(changeRequest.subject, changeRequest.data)
+      }
+      case DELETE_SCHEMA_ACTION => {
+        srClient.deleteSchema(changeRequest.subject, changeRequest.id.toString)
+      }
+      case SET_COMPAT_LEVEL_ACTION => {
+        srClient.setTopLevelCompatibility(changeRequest.data)
+      }
+    }
+
+    val responseJSON: JValue = parse(responseJsonText)
+
+    fileStatusKeeper.update(
+      subject = changeRequest.subject,
+      action = changeRequest.action,
+      data = changeRequest.data,
+      responseJSON = responseJSON)
+  }
+
+}
+
+object KafkaExecutor {
+
+  val CREATE_TOPIC_ACTION: String = "create-topic"
+  val DELETE_TOPIC_ACTION: String = "delete-topic"
+
+  def apply(adminClient: AdminClient) = new KafkaExecutor(adminClient)
+}
+
+class KafkaExecutor(adminClient: AdminClient) extends Executor {
+
+  import KafkaExecutor._
+
+  def execute(changeRequest: BrokerChangeRequest) : Unit = {
+
+    changeRequest.action match {
+      case CREATE_TOPIC_ACTION => {
+        val numPartitions: Int = changeRequest.config.numPartitions
+        val replicationFactor: Short = changeRequest.config.replicationFactor
+        val newTopic = new NewTopic(changeRequest.topic, numPartitions, replicationFactor)
+        adminClient.createTopics(Collections.singleton(newTopic))
+      }
+      case DELETE_TOPIC_ACTION => {
+        adminClient.deleteTopics(Collections.singleton(changeRequest.topic))
+      }
+    }
+  }
+}
--- src/main/scala/com/purbon/kafka/services/MigrationService.scala
+++ src/main/scala/com/purbon/kafka/services/MigrationService.scala
@@ -1,12 +1,9 @@
 package com.purbon.kafka.services
 
-import java.util.Collections
-
+import com.purbon.kafka.executors.{KafkaExecutor, SchemaRegistryExecutor}
 import com.purbon.kafka.readers.{BrokerChangeRequest, ChangeRequestReader, SchemaRegistrySingleChangeRequest}
 import com.purbon.kafka.{FileStatusKeeper, SchemaRegistryClient}
-import org.apache.kafka.clients.admin.{AdminClient, NewTopic}
-import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.apache.kafka.clients.admin.AdminClient
 
 class MigrationService(schemaRegistryClient: SchemaRegistryClient,
                        adminClient: AdminClient,
@@ -19,46 +16,14 @@ class MigrationService(schemaRegistryClient: SchemaRegistryClient,
       if (!changeRequest.name.startsWith("#")) {
         println(changeRequest.name)
         changeRequest match {
-          case scr: SchemaRegistrySingleChangeRequest => execute(scr)
-          case bcr: BrokerChangeRequest => executeKafkaRequest(bcr)
+          case scr: SchemaRegistrySingleChangeRequest => {
+            SchemaRegistryExecutor(schemaRegistryClient, fileStatusKeeper).execute(scr)
+          }
+          case bcr: BrokerChangeRequest => KafkaExecutor(adminClient).execute(bcr)
         }
 
       }
     }
   }
 
-  private def execute(changeRequest: SchemaRegistrySingleChangeRequest): Unit = {
-    //println(s"${changeRequest.action} ${changeRequest.data} ${changeRequest.subject}")
-    val responseJsonText = changeRequest.action match {
-      case "register" => {
-       schemaRegistryClient.addSchema(changeRequest.subject, changeRequest.data)
-      }
-      case "delete" => {
-        schemaRegistryClient.deleteSchema(changeRequest.subject, changeRequest.id.toString)
-      }
-      case "set_top_level_compatibility" => {
-        schemaRegistryClient.setTopLevelCompatibility(changeRequest.data)
-      }
-    }
-    val responseJSON: JValue = parse(responseJsonText)
-    fileStatusKeeper.update(subject = changeRequest.subject,
-                            action = changeRequest.action,
-                            data = changeRequest.data,
-                            responseJSON = responseJSON)
-  }
-
-  private def executeKafkaRequest(changeRequest: BrokerChangeRequest) : Unit = {
-    println(changeRequest.action)
-    changeRequest.action match {
-      case "create-topic" => {
-        val numPartitions: Int = changeRequest.config.numPartitions
-        val replicationFactor: Short = changeRequest.config.replicationFactor
-        val newTopic = new NewTopic(changeRequest.topic, numPartitions, replicationFactor)
-        adminClient.createTopics(Collections.singleton(newTopic))
-      }
-      case "delete-topic" => {
-        adminClient.deleteTopics(Collections.singleton(changeRequest.topic))
-      }
-    }
-  }
 }
