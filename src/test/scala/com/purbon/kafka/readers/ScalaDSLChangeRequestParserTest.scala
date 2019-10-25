720f38c723229bb6e801ee34eb93fd05550b3701
Pere Urbon <pere@confluent.io>
2019-10-26 00:26:25 +0200

    add test for parsing the down action in a migration

--- src/test/scala/com/purbon/kafka/readers/ScalaDSLChangeRequestParserTest.scala
+++ src/test/scala/com/purbon/kafka/readers/ScalaDSLChangeRequestParserTest.scala
@@ -17,56 +17,68 @@ class ScalaDSLChangeRequestParserTest  extends FunSpec
 
   describe("A change request parser") {
 
+    val data =
+      """
+        |import com.purbon.kafka.parsers.{MigrationClients, SchemaMigration}
+        |
+        |class SchemaSetupMigration(clients: MigrationClients) extends SchemaMigration(clients) {
+        |
+        |  def up(): Unit = {
+        |    val schema = Map( "schema" -> Map( "type" -> "string ") )
+        |    register("kafka-key2", schema)
+        |  }
+        |
+        |  def down(): Unit = {
+        |    remove("kafka-key2", "latest")
+        |  }
+        |}
+        |scala.reflect.classTag[SchemaSetupMigration].runtimeClass
+      """.stripMargin
+
     it("should be able to perform up actions") {
-
-      val data =
-        """
-          |import com.purbon.kafka.parsers.{MigrationClients, SchemaMigration}
-          |
-          |class SchemaSetupMigration(clients: MigrationClients) extends SchemaMigration(clients) {
-          |
-          |  def up(): Unit = {
-          |    val schema = Map( "schema" -> Map( "type" -> "string ") )
-          |    register("kafka-key2", schema)
-          |  }
-          |
-          |  def down(): Unit = {
-          |    remove("kafka-key2", "latest")
-          |  }
-          |}
-          |scala.reflect.classTag[SchemaSetupMigration].runtimeClass
-         """.stripMargin
-
       when(mockClient.addSchema(eqTo("kafka-key2"), any[String])) thenReturn "foo"
       migrationParser.parse(data).up
       verify(mockClient, times(1)).addSchema(eqTo("kafka-key2"), any[String])
-
     }
+
+    it("should be able to perform down actions") {
+      when(mockClient.deleteSchema(eqTo("kafka-key2"), eqTo("latest"))) thenReturn "foo"
+      migrationParser.parse(data).down
+      verify(mockClient, times(1)).deleteSchema(eqTo("kafka-key2"), eqTo("latest"))
+    }
+
 
     describe("A broker change request parser") {
 
-      it ("should be able to perform up action") {
+      val data =
+        """
+          |import com.purbon.kafka.parsers.{MigrationClients, TopicMigration}
+          |
+          |class CreateTopicMigration(clients: MigrationClients) extends TopicMigration(clients) {
+          |
+          |  def up(): Unit = {
+          |    createTopic("foo", 1, 1)
+          |  }
+          |
+          |  def down(): Unit = {
+          |    deleteTopic("foo")
+          |  }
+          |}
+          |scala.reflect.classTag[CreateTopicMigration].runtimeClass
+        """.stripMargin
 
-        val data =
-          """
-            |import com.purbon.kafka.parsers.{MigrationClients, TopicMigration}
-            |
-            |class CreateTopicMigration(clients: MigrationClients) extends TopicMigration(clients) {
-            |
-            |  def up(): Unit = {
-            |    createTopic("foo", 1, 1)
-            |  }
-            |
-            |  def down(): Unit = {
-            |    deleteTopic("foo")
-            |  }
-            |}
-            |scala.reflect.classTag[CreateTopicMigration].runtimeClass
-          """.stripMargin
+      it ("should be able to perform up action") {
 
         doNothing.when(mockAdminClient).createTopic("foo", 1, 1)
         migrationParser.parse(data).up
         verify(mockAdminClient, times(1)).createTopic("foo", 1, 1)
+      }
+
+      it ("should be able to perform down action") {
+
+        doNothing.when(mockAdminClient).deleteTopic("foo")
+        migrationParser.parse(data).down
+        verify(mockAdminClient, times(1)).deleteTopic("foo")
       }
 
     }
