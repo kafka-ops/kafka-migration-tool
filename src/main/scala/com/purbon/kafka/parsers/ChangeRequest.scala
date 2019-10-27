69831c6c5085c7d40e69a93c44c93917410a07f6
Pere Urbon <pere@confluent.io>
2019-10-27 08:18:34 +0100

    dry out the methods for handling the schema registry

--- src/main/scala/com/purbon/kafka/parsers/ChangeRequest.scala
+++ src/main/scala/com/purbon/kafka/parsers/ChangeRequest.scala
@@ -23,18 +23,26 @@ class MigrationClients(var schemaRegistry: SchemaRegistryClient, var adminClient
 
 }
 
-abstract class SchemaMigration(clients: MigrationClients) extends Migration {
+trait NiceSchemaMigrationOperations {
 
   implicit val formats = DefaultFormats
 
-  def register(subject: String, schema: Map[String, Any]): Unit = {
+  def register(subject: String, schema: Map[String, Any])(implicit client: SchemaRegistryClient): Unit = {
     println(s"register ${subject}")
-    clients.schemaRegistry.addSchema(subject, write(schema))
+    client.addSchema(subject, write(schema))
   }
-  def remove(subject: String, version: String): Unit = {
+  def remove(subject: String, version: String)(implicit client: SchemaRegistryClient): Unit = {
     println(s"remove ${subject}")
-    clients.schemaRegistry.deleteSchema(subject, version)
+    client.deleteSchema(subject, version)
   }
+}
+
+abstract class SchemaMigration(clients: MigrationClients)
+  extends Migration
+  with NiceSchemaMigrationOperations {
+
+  implicit val client = clients.schemaRegistry
+
 }
 
 
