6b56705790a75bab868d46fcab3a3c70090b8280
Pere Urbon <pere@confluent.io>
2019-11-09 19:43:37 +0100

    add missing ACL for consumer group describe for schema registry

--- src/main/scala/com/purbon/kafka/Config.scala
+++
@@ -1,9 +0,0 @@
-package com.purbon.kafka
-
-case class Config(
-                   migrationsURI: String = "migrations",
-                   brokersUrl: String = "localhost:9092",
-                   schemaRegistryUrl: String = "http://localhost:8081",
-                   action: Option[String] = None,
-                   migrationType: Option[String] = None
-                 )
